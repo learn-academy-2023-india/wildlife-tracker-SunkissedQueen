4d84c751c5bf3117b362ede4a4f935d3210bbcc7
SunkissedQueen <baxtercm@outlook.com>
2024-01-31 15:11:46 -0800

    create API endpoint

--- app/controllers/chicken_coops_controller.rb
+++ app/controllers/chicken_coops_controller.rb
@@ -7,4 +7,18 @@ class ChickenCoopsController < ApplicationController
     chicken = ChickenCoop.find(params[:id])
     render json: chicken
   end
+  def create
+    chicken = ChickenCoop.create(chicken_params)
+    if chicken.valid?
+      render json: chicken
+    else
+      render json: chicken.errors
+    end
+  end
+
+  private
+  def chicken_params
+    params.require(:chicken_coop).permit(:name, :variant, :origin)
+  end
+
 end
